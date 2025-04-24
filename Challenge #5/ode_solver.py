6149e91476b4caed24de72cc257c7c25e1d2b5d0
jtristan123 <43759132+jtristan123@users.noreply.github.com>
2025-04-23 19:16:16 -0700

    Create ode_solver.py

---
+++ Challenge #5/ode_solver.py
@@ -0,0 +1,23 @@
+from scipy.integrate import solve_ivp
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the differential equation dy/dt = -2y
+def dydt(t, y):
+    return -2 * y
+
+# Time range and initial condition
+t_span = (0, 5)
+y0 = [1]
+
+# Solve the ODE
+solution = solve_ivp(dydt, t_span, y0, t_eval=np.linspace(0, 5, 100))
+
+# Plot the result
+plt.plot(solution.t, solution.y[0], label='Numerical solution')
+plt.xlabel('Time t')
+plt.ylabel('y(t)')
+plt.title('Solving dy/dt = -2y using solve_ivp')
+plt.legend()
+plt.grid()
+plt.show()
